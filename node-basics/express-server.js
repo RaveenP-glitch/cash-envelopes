86bfa5ac85674330996df6221706c0269838f7b8
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2025-02-21 00:34:27 +0530

    Feat: Creating the my personal blog project using nextjs and sanity

--- node-basics/express-server.js
+++ node-basics/express-server.js
@@ -1,20 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-    console.log('Middleware request received');
-    next();
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
-});
 
-app.get('/name', (req, res) => {
-    res.send('Hello from Express.js');
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 app.listen(port, () => {
